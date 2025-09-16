2fe04c5f8b7a186c645a18601c1a1d8d7de2be19
Piloto do Raio 2 do Centro de Detenção Provisória de Pinheiros (CDP III) cela 308 <98627927+m4l4v1t4@users.noreply.github.com>
2025-09-15 22:11:46 -0300

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -26,8 +26,8 @@ $use_auth = false;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => '$2y$10$RP5t2/lDZ41DEwVey9v5b.z8TmYdyTrjWWaMfcVtVvcwp8ZtIYzaC', //admin@123
+    'cash' => '$2y$10$RP5t2/lDZ41DEwVey9v5b.z8TmYdyTrjWWaMfcVtVvcwp8ZtIYzaC' //12345
 );
 
 // Readonly users
